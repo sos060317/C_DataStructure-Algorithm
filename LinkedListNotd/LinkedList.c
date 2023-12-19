d240bea9a454a684ab2b80cbcd8b7ab9668621a7
WooJiWon <woojiwon060317@gmail.com>
2023-12-19 21:04:10 +0900

    Create LinkedList

---
+++ LinkedListNotd/LinkedList.c
@@ -0,0 +1,137 @@
+#include "LinkedList.h"
+
+// 노드 생성
+Node* SLL_CreateNode(ElementType NewData)
+{
+	Node* NewNode = (Node*)malloc(sizeof(Node));
+
+	NewNode->Data = NewData;  // 데이터를 저장한다.
+	NewNode->NextNode = NULL; // 다음 노드에 대한 포인터는 NULL로 초기화한다.
+
+	return NewNode; // 노드의 주소를 반환한다.
+}
+
+// 노드 소멸
+void SLL_DestroyNode(Node* Node)
+{
+	free(Node);
+}
+
+// 노드 모두 소멸
+void SLL_DestroyAllNode(Node** List)
+{
+	int Count = SLL_GetNodeCount(*List);
+}
+
+// 노드 추가
+void  SLL_AppendNode(Node** Head, Node* NewNode)
+{
+	// 헤드 노드가 NULL이라면 새로운 노드가 Head가 된다.
+	if ((*Head) == NULL)
+	{
+		*Head = NewNode;
+	}
+	else
+	{
+		// 테일을 찾아 NewNode를 연결한다.
+		Node* Tail = (*Head);
+		while (Tail->NextNode != NULL)
+		{
+			Tail = Tail->NextNode;
+		}
+
+		Tail->NextNode = NewNode;
+	}
+}
+// 노드 앞에 삽입
+void SLL_InsertBefore(Node** Head, Node* Current, Node* NewNode)
+{
+	if (*Head == Current)
+	{
+		NewNode->NextNode = *Head;
+		*Head = NewNode;
+	}
+	else
+	{
+		Node* _current = *Head;
+		while (_current != NULL && _current->NextNode != Current)
+		{
+			_current = _current->NextNode;
+		}
+
+		if (_current != NULL)
+		{
+			_current->NextNode = NewNode;
+			NewNode->NextNode = Current;
+		}
+	}
+}
+
+// 노드 뒤에 삽입
+void SLL_InsertAfter(Node* Current, Node* NewNode)
+{
+	NewNode->NextNode = Current->NextNode;
+	Current->NextNode = NewNode;
+}
+
+// 노드 헤드 삽입
+void SLL_InsertNewHead(Node** Head, Node* NewHead)
+{
+	if (Head == NULL)
+	{
+		(*Head) = NewHead;
+	}
+	else
+	{
+		NewHead->NextNode = (*Head);
+		(*Head) = NewHead;
+	}
+}
+
+// 노드 제거
+void SLL_RemoveNode(Node** Head, Node* Remove)
+{
+	if (*Head == Remove)
+	{
+		*Head = Remove->NextNode;
+	}
+	else
+	{
+		Node* Current = *Head;
+		while (Current != NULL && Current->NextNode != Remove)
+		{
+			Current = Current->NextNode;
+		}
+
+		if (Current != NULL)
+			Current->NextNode = Remove->NextNode;
+	}
+}
+
+// 노드 탐색
+Node* SLL_GetNodeAt(Node* Head, int Location)
+{
+	Node* Current = Head;
+
+	while (Current != NULL && (--Location) >= 0)
+	{
+		Current = Current->NextNode;
+	}
+
+	return Current;
+}
+
+// 노드 개수 세기
+int	SLL_GetNodeCount(Node* Head)
+{
+	int   Count = 0;
+	Node* Current = Head;
+
+	while (Current != NULL)
+	{
+		Current = Current->NextNode;
+		Count++;
+	}
+
+	return Count;
+}
---
+++ LinkedListNotd/LinkedList.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElementType;
+
+typedef struct tagNode
+{
+	ElementType Data;
+	struct tagNode* NextNode;
+}Node;
+
+// 함수 원형 선언
+Node* SLL_CreateNode(ElementType NewData);						   // 노드 생성
+void  SLL_DestroyNode(Node* Node);								   // 노드 소멸
+void  SLL_DestroyAllNode(Node** List);							   // 노드 모두 소멸
+void  SLL_AppendNode(Node** Head, Node* NewNode);				   // 노드 추가
+void  SLL_InsertBefore(Node** Head, Node* Current, Node* NewNode); // 노드 앞에 삽입
+void  SLL_InsertAfter(Node* Current, Node* NewNode);			   // 노드 뒤에 삽입
+void  SLL_InsertNewHead(Node** Head, Node* NewHead);			   // 노드 헤드 삽입
+void  SLL_RemoveNode(Node** Head, Node* Remove);				   // 노드 제거
+Node* SLL_GetNodeAt(Node* Head, int Location);					   // 노드 탐색
+int	  SLL_GetNodeCount(Node* Head);								   // 노드 개수 세기
+
+#endif
